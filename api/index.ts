eed396912d03474f3f837c77d6e46e97f15c5120
Kennedy Kinyanjui <kennedykinyanjui@outlook.com>
2024-03-10 18:59:13 +0300

    initial commit

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+    // res.send("Express on Vercel")
+const ip = req.ip.replace(/^.*:/, ''); // Extract IP address from req.ip
+  const reversedIp = ip.split('.').reverse().join('.');
+  res.send(`Reversed IP Address: ${reversedIp}`);
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
